a6f925698ca61e492fe3e85d7c148385f3a18b5e
bumirbayev <28890261+bumirbayev@users.noreply.github.com>
2021-03-22 23:50:36 +0600

    Create datatypes_operators_python.py

---
+++ datatypes_operators_python.py
@@ -0,0 +1,25 @@
+# data types: numbers and string and boolean
+
+a = 3 + 4
+b = 6 - 7
+print(6/3)
+
+print(4 > 5)
+# should return False
+
+print(4 > 5 or 3 < 4)
+# returns True because one of them is true
+
+# False or True returns True
+# False and True returns False
+# True and True returns True
+
+return("4" + "4") # returns 44
+str mystring  = "g" + "4" + "a"
+print(mystring)
+mystring[0] # shows you 'g'
+
+# run all thiis in python console if you have py charm or repl's terminal
+
+type("sadasdasd") #returns str which means string
+type(2) #returns int which means integer
