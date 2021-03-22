568ab42c938f771e56622ebafc5d2f11227b5cca
bumirbayev <28890261+bumirbayev@users.noreply.github.com>
2021-03-22 23:37:21 +0600

    Update variables_python.py

--- variables_python.py
+++ variables_python.py
@@ -1,3 +1,26 @@
-# Variables 
-a = 5
-a = 2
+# Variables
+
+a = 9
+b = 4
+
+# play in python console
+# a + b should return 14
+# a = b, and then type in console a or b
+
+# How to learn programming is to play
+# in programming language, syntax is the rules that computer understands
+
+# comments is the contacts that other programmers understand and read
+
+# my age
+age = 25
+
+# when am I born
+print(2021-age)
+
+# Year when it was legal to drink
+print(2021 - age + 21)
+
+# minimum age he can date (how i met your mother)
+print(age/2 +7)
+
