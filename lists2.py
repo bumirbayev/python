3da0d2b397b8625a766d2505a2272a74686834ae
bumirbayev <28890261+bumirbayev@users.noreply.github.com>
2021-03-25 23:20:50 +0600

    Add files via upload

---
+++ lists2.py
@@ -0,0 +1,20 @@
+# lists
+# Problem 1: Write a function that prints each element in the list on a separate line:
+def print_list(lst):
+    print(lst[0])
+    return
+# type here commands like lst[0], lst[1], lst[2]
+    """ while i < len(lst):
+        print(i)
+        i += 1
+        pr
+
+    return
+    """
+
+"""
+>print_list([1,2,3])
+1
+2
+3
+"""
