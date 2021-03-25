7c14964eee6892129e003167cbc9ef5423fe10d1
bumirbayev <28890261+bumirbayev@users.noreply.github.com>
2021-03-25 23:20:26 +0600

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,13 @@
+# Lists
+
+nums = [2, 5, 14, 17, 20, 35]
+
+# search add item to list python 3, most website good enough, docs.python.org
+# nums.append(14)
+# nums.insert(0, 5) insert in 0th place the number 5
+# nums.remove(14) removes 14
+# nums.pop(0) removes first item in list
+# nums.pop() removes last item in list
+# len(nums) calculates the length of nums, not only numbers but strings as well xD
+# search for another functions, you need understanding of python, you don't need to memorize python functions)))
+# so you can understand any language
