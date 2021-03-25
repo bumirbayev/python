920af0c2fdafaf79816cc01cd63e71e743a71355
bumirbayev <28890261+bumirbayev@users.noreply.github.com>
2021-03-25 23:21:11 +0600

    Add files via upload

---
+++ list3while.py
@@ -0,0 +1,10 @@
+# lists
+# Problem 1: Write a function that prints each element in the list on a separate line:
+def print_list(lst):
+    i = 0
+    while i < len(lst):
+        print(i)
+        i += 1
+    print("done")
+
+# print_list([2,2,2,2])
