aae7f3c6fcbddad69ec0103843230b4854044ca7
bumirbayev <28890261+bumirbayev@users.noreply.github.com>
2021-03-31 01:21:49 +0600

    Add files via upload

---
+++ while sum and square.py
@@ -0,0 +1,18 @@
+# Lists
+
+def square_list(nums):
+    i = 0
+    while i < len(nums):
+        nums[i] = nums[i] * nums[i]
+        i += 1
+    return nums
+
+# Return the sum of all the numbers in the list
+
+def sum_list(nums):
+    total = 0
+    i = 0
+    while i < len(nums):
+        total += nums[i]
+        i += 1
+    return total
